c483c0bbc8af320cfea262d70af24c33874beb18
Charlie Chale <chalemcchale98@gmail.com>
2022-07-19 00:20:04 +0300

    For Tony Pale

---
+++ PTSLibrary/PTSCustomerFacade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary
+{
+    class PTSCustomerFacade : PTSSuperFacade
+    {
+        private DAO.CustomerDAO dao;
+
+        public PTSCustomerFacade() : base(new PTSLibrary.DAO.CustomerDAO)
+        {
+            dao = new DAO.CustomerDAO();    
+        }
+
+        public new Project[] GetListOfTasks(Guid projectId)
+        {
+
+
+            return (dao.GetListOfProjects(customerId)).ToArray();
+        }
+
+    }
+}
